f30aeb60f97175b2c089e0dd9e18f35a07888426
Gerardo Salazar <gsalaz9800@gmail.com>
2018-12-06 18:23:20 -0600

    Delete frontend to make way for git submodule

--- backend/frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-import calories.views
-import frontend.views
-
-urlpatterns = [
-    path('', frontend.views.index),
-    path('counter/', calories.views.calories),
-]
